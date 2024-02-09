29b9c0cf2cf6ce77d7c3a1bd04940b9c36fca78a
Mohit Kumar <129601983+er-mohitkumar@users.noreply.github.com>
2024-02-10 00:37:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,24 +9,24 @@ function selectDept() {
         select.value = 'CS';
     }
 }
-
+function load() {
+    document.getElementById('loadingIcon').style.display = 'block';
+    document.getElementById('webBody').style.display = 'none';
+    document.body.style.minHeight = '100vh';
+}
+function dload() {
+    document.getElementById('loadingIcon').style.display = 'none';
+    document.getElementById('webBody').style.display = 'block';
+    document.body.style.minHeight = '0';
+}
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     const year = document.getElementById("year").value;
     const dept = document.getElementById("dept").value;
-    const load = true;
-    if (load) {
-        document.getElementById('loadingIcon').style.display = 'block';
-        document.getElementById('webBody').style.display = 'none';
-        document.body.style.minHeight = '100vh';
-    }
+    load();
     window.location.href = `${dept}/${year}/${year}.html`;
-    if (window.location.href === `${dept}/${year}/${year}.html`) {
-        document.getElementById('loadingIcon').style.display = 'none';
-        document.getElementById('webBody').style.display = 'block';
-        document.body.style.minHeight = '0';
-    }
+    dload();
 })
 
 
